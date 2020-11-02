49a1689fc160101d24a14cf9a831c6058302dc5d
KazutoYunoki <kazuto233@gmail.com>
2020-11-02 21:41:55 +0900

    Fractionの抽象クラスと仮分数クラスImproperFractionの実装

--- src/Fraction2.java
+++ src/Fraction2.java
@@ -3,48 +3,47 @@
  * @author kazut
  *
  */
-public class Fraction implements Comparable<Fraction>{
+public class Fraction2 implements Comparable<Fraction2>, Calculate {
 	/**　分子*/
-	private int mol;
+	final protected int mol;
 	/**　分母*/
-	private int den;
+	final protected int den;
+
 	/**
 	 * コンストラクタ
 	 * @param num　整数
 	 */
-	public Fraction(int num) {
+	public Fraction2(int num) {
 		this(num, 1);
 	}
+
 	/**
 	 * コンストラクタ（分母に0が設定された場合例外投げる）
 	 * @param mol 分子
 	 * @param den　分母
 	 */
-	public Fraction(int mol, int den)
-	{
+	public Fraction2(int mol, int den) {
 		//　もし分母にゼロがある場合(例外を投げる）
-		if(den == 0)
-		{
+		if (den == 0) {
 			throw new ArithmeticException();
 		}
 		//　分母に負数がある場合分子に負数を持っていく
-		if(den < 0)
-		{
+		if (den < 0) {
 			this.mol = -mol;
 			this.den = -den;
-		}
-		else
-		{
+		} else {
 			this.mol = mol;
 			this.den = den;
 		}
 	}
+
 	/**
 	 * 分数同士の足し算
 	 * @param f　加数
 	 * @return　加算結果
 	 */
-	public Fraction add(Fraction f) {
+	@Override
+	public Fraction2 add(Fraction2 f) {
 
 		//　通分処理
 		int mol1 = this.getMol() * f.getDen();
@@ -56,142 +55,130 @@ public class Fraction implements Comparable<Fraction>{
 		int mol = mol1 + mol2;
 
 		//　加算結果の分数を作成
-		Fraction re = new Fraction(mol, den1);
+		Fraction2 re = new Fraction2(mol, den1);
 
-		//　約分する
-		re = re.reduction();
-		return re;
+		return re.reduce();
 	}
+
 	/**
 	 * 分数と整数同士の足し算
 	 * @param num　整数
 	 * @return　加算結果
 	 */
-	public Fraction add(int num)
-	{
+	@Override
+	public Fraction2 add(int num) {
 		//　受け取った整数（int)をFraction型へ
-		Fraction f = new Fraction(num);
+		Fraction2 f = new Fraction2(num);
 		//　分数同士の足し算を実施
-		Fraction re = this.add(f);
+		Fraction2 re = this.add(f);
 
 		return re;
 	}
+
 	/**
 	 * 分数同士の引き算
 	 * @param f　引数
 	 * @return　減算結果
 	 */
-	public Fraction sub(Fraction f)
-	{
-		int mol1 = this.getMol() * f.getDen();
-		int den1 = this.getDen() * f.getDen();
+	@Override
+	public Fraction2 sub(Fraction2 f) {
+		//　引数を-1倍にして加算
+		Fraction2 min = f.multi(-1);
 
-		int mol2 = f.getMol() * this.getDen();
+		Fraction2 re = this.add(min);
 
-		//　分子同士を引き算
-		int mol = mol1 - mol2;
-
-		//　加算結果の分数を作成
-		Fraction re = new Fraction(mol, den1);
-
-		//　約分する
-		re = re.reduction();
-		return re;
+		return re.reduce();
 	}
+
 	/**
 	 * 分数と整数の引き算
 	 * @param num
 	 * @return　計算結果
 	 */
-	public Fraction sub(int num)
-	{
+	@Override
+	public Fraction2 sub(int num) {
 		//　受け取った整数（int)をFraction型へ
-		Fraction f = new Fraction(num);
+		Fraction2 f = new Fraction2(num);
 		//　分数同士の引き算を実施
-		Fraction re = this.sub(f);
+		Fraction2 re = this.sub(f);
 
 		return re;
 	}
+
 	/**
 	 * 分数同士の掛け算
 	 * @param f かける数
 	 * @return　乗算結果
 	 */
-	public Fraction multi(Fraction f)
-	{
+	@Override
+	public Fraction2 multi(Fraction2 f) {
 		//　分子と分母を掛け算する
 		int mol = this.getMol() * f.getMol();
 		int den = this.getDen() * f.getDen();
 
-		Fraction re = new Fraction(mol, den);
-		re = re.reduction();
+		Fraction2 re = new Fraction2(mol, den);
 
-		return re;
+		return re.reduce();
 	}
+
 	/**
 	 * 分数と整数の掛け算
 	 * @param num 整数
 	 * @return　乗算結果
 	 */
-	public Fraction multi(int num)
-	{
-		int mol = this.getMol() * num;
-		int den = this.getDen();
+	@Override
+	public Fraction2 multi(int num) {
+		//　整数を分数に変換
+		Fraction2 f = new Fraction2(num);
 
-		Fraction re = new Fraction(mol, den);
-		re = re.reduction();
+		//　分数同士の掛け算
+		Fraction2 re = this.multi(f);
 
-		return re;
+		return re.reduce();
 	}
+
 	/**
 	 * 分数同士の割り算
 	 * @param f　割る数
 	 * @return　剰余結果
 	 */
-	public Fraction div(Fraction f)
-	{
+	@Override
+	public Fraction2 div(Fraction2 f) {
 		//　逆数の分数を作成
-		Fraction inv = new Fraction(f.getDen(), f.getMol());
+		Fraction2 inv = new Fraction2(f.getDen(), f.getMol());
 
 		//　逆数と乗算
-		Fraction re = this.multi(inv);
+		Fraction2 re = this.multi(inv);
 
 		return re;
 
 	}
+
 	/**
 	 * 分数と整数の割り算
 	 * @param num　割る数
 	 * @return　剰余結果
 	 */
-	public Fraction div(int num) throws ArithmeticException
-	{
+	@Override
+	public Fraction2 div(int num) throws ArithmeticException {
 		//　0で割る場合は例外処理
-		if(num == 0)
-		{
+		if (num == 0) {
 			throw new ArithmeticException();
 		}
 		//　整数の逆数を作成
-		Fraction f = new Fraction(1, num);
+		Fraction2 f = new Fraction2(1, num);
 
 		//　逆数と掛け算
-		Fraction re = this.multi(f);
+		Fraction2 re = this.multi(f);
 
 		return re;
 	}
+
 	/**
 	 * 分数を約分する関数
 	 * @return 約分された分数
 	 */
-	public Fraction reduction() {
-
-		//　分母の値と分子の値を取得
-		int a = this.getDen();
-		int b = this.getMol();
-
-		//　負数の場合正の数に変換
-		a = a < 0 ? -a : a;
-		b = b < 0 ? -b : b;
+	public Fraction2 reduce() {
 
 		//　最大公約数を取得
 		int g = gcd(this.den, this.mol);
@@ -200,74 +187,75 @@ public class Fraction implements Comparable<Fraction>{
 		int d = this.den / g;
 		int m = this.mol / g;
 
-
 		//　約分した分数を作成し返す
-		Fraction f = new Fraction(m, d);
+		Fraction2 f = new Fraction2(m, d);
 		return f;
 	}
+
 	/**
 	 * 分数を浮動小数点に変換
 	 * @return
 	 */
-	public double convertFloat()
-	{
-		double f = (double)this.getMol() / this.getDen();
+	public double convertFloat() {
+		double f = (double) this.getMol() / this.getDen();
 		return f;
 	}
+
 	/**
 	 * 整数に変換、できなければ例外処理
 	 * @return　整数
 	 */
-	public int convertInt() throws ArithmeticException
-	{
+	public int convertInt() throws ArithmeticException {
 
 		//　分子を分母で割り切れる場合整数に変換
-		if(this.getMol() % this.getDen() == 0)
-		{
+		if (this.getMol() % this.getDen() == 0) {
 			int num = this.getMol() / this.getDen();
 			return num;
-		}
-		else
-		{
+		} else {
 			//　整数に変換できない場合は例外を投げる
 			throw new ArithmeticException();
 		}
 	}
+
 	/**
 	 * 2つの整数から最大公約数を求める関数
 	 * @param a　整数
 	 * @param b　整数
 	 * @return　最大公約数
 	 */
-	private int gcd(int a, int b) {
+	private int gcd(int x, int y) {
 		int r;
-		if(a < b) {
+
+		//　負数の場合正の数に変換
+		int a = x < 0 ? -x : x;
+		int b = y < 0 ? -y : y;
+
+		if (a < b) {
 			int tmp = a;
 			a = b;
 			b = tmp;
 		}
-		while((r = a % b) != 0)
-		{
+		while ((r = a % b) != 0) {
 			a = b;
 			b = r;
 		}
 
 		return b;
 	}
+
 	/**
 	 * 分子の値を取得
 	 * @return　分子
 	 */
-	public int getMol()
-	{
+	public int getMol() {
 		return mol;
 	}
+
 	/**
 	 * 分母の値を取得
 	 * @return　分母
 	 */
-	public int getDen()
-	{
+	public int getDen() {
 		return den;
 	}
 
@@ -275,15 +263,13 @@ public class Fraction implements Comparable<Fraction>{
 	public String toString() {
 
 		//　整数でない場合
-		if(this.den != 1)
-		{
+		if (this.den != 1) {
 			return String.valueOf(this.mol) + '/' + String.valueOf(this.den);
-		}
-		else
-		{
+		} else {
 			return String.valueOf(this.mol);
 		}
 	}
+
 	/**
 	 * hashCode()のオーバーライド
 	 */
@@ -293,50 +279,47 @@ public class Fraction implements Comparable<Fraction>{
 		int result = 1;
 
 		//　約分してからハッシュ値を計算(同じ値の分数を同じハッシュ値にするため）
-		Fraction f = this.reduction();
+		Fraction2 f = this.reduce();
 		result = prime * result + f.getDen();
 		result = prime * result + f.getMol();
 		return result;
 	}
+
 	/**
 	 * equalsメソッドのオーバーライド
 	 */
 	@Override
 	public boolean equals(Object o) {
 		//　オブジェクトが同じ参照ならばtrue
-		if(o == this) {
+		if (o == this) {
 			return true;
 		}
 		//　オブジェクトの型がFractionでなければfalse
-		if(!(o instanceof Fraction))
-		{
+		if (!(o instanceof Fraction2)) {
 			return false;
 		}
 		//　比較対象の分数を約分
-		Fraction f = (Fraction) o;
-		f = f.reduction();
+		Fraction2 f = (Fraction2) o;
+		f = f.reduce();
 
 		//　比較する分数の約分
-		Fraction f2 = this.reduction();
+		Fraction2 f2 = this.reduce();
 
 		//　分子同士、分母同士の比較結果を返す
 		return (f.getMol() == f2.getMol()) && (f.getDen() == f2.getDen());
 	}
+
 	/**
 	 * compareToメソッドのオーバーライド
 	 */
 	@Override
-	public int compareTo(Fraction f) {
+	public int compareTo(Fraction2 f) {
 		//　分母を合わせて分子による比較
 		int mol1 = this.getMol() * f.getDen();
 
 		int mol2 = f.mol * this.getDen();
 
- 		return mol1 - mol2;
+		return mol1 - mol2;
 	}
 
-
-
-
 }
-
--- src/Fraction2Test.java
+++ src/Fraction2Test.java
@@ -3,122 +3,122 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class FractionTest {
+public class Fraction2Test {
 
 	@Test(expected = ArithmeticException.class)
 	public void testConstract() {
-		Fraction f = new Fraction(1, 0);
+		new Fraction2(1, 0);
 
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test
 	public void testAdd() {
-		Fraction f = new Fraction(1, 2);
-		Fraction f2 = new Fraction(-1, 3);
+		Fraction2 f = new Fraction2(1, 2);
+		Fraction2 f2 = new Fraction2(-1, 3);
 		int num = 2;
 
-		Fraction r = f.add(f2);
+		Fraction2 r = f.add(f2);
 		assertThat("1/6", is(r.toString()));
 
-		Fraction r1 = f.add(num);
+		Fraction2 r1 = f.add(num);
 		assertThat("5/2", is(r1.toString()));
 
-
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testSub()
-	{
-		Fraction f = new Fraction(1, 2);
-		Fraction f2 = new Fraction(1, 3);
+	public void testSub() {
+		Fraction2 f = new Fraction2(1, 2);
+		Fraction2 f2 = new Fraction2(1, 3);
 		int num = 2;
 
-		Fraction r = f.sub(f2);
+		Fraction2 r = f.sub(f2);
 		assertThat("1/6", is(r.toString()));
 
-		Fraction r1 = f.sub(num);
+		Fraction2 r1 = f.sub(num);
 		assertThat("-3/2", is(r1.toString()));
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testMulti()
-	{
-		Fraction f = new Fraction(1, 2);
-		Fraction f2 = new Fraction(1, 3);
+	public void testMulti() {
+		Fraction2 f = new Fraction2(1, 2);
+		Fraction2 f2 = new Fraction2(1, 3);
 		int num = 2;
 
-		Fraction r = f.multi(f2);
+		Fraction2 r = f.multi(f2);
 		assertThat("1/6", is(r.toString()));
 
-		Fraction r1 = f.multi(num);
+		Fraction2 r1 = f.multi(num);
 		assertThat("1", is(r1.toString()));
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test(expected = ArithmeticException.class)
-	public void testDiv(){
-		Fraction f = new Fraction(1, 2);
-		Fraction f2 = new Fraction(1, 3);
+	public void testDiv() {
+		Fraction2 f = new Fraction2(1, 2);
+		Fraction2 f2 = new Fraction2(1, 3);
 
-		Fraction r = f.div(f2);
+		Fraction2 r = f.div(f2);
 		assertThat("3/2", is(r.toString()));
 
-		Fraction r1 = f.div(2);
+		Fraction2 r1 = f.div(2);
 		assertThat("1/4", is(r1.toString()));
 
-		Fraction r2 = f.div(0);
-
+		f.div(0);
 
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testReduction() {
-		Fraction f = new Fraction(3, 6);
-		f = f.reduction();
+	public void testreduce() {
+		Fraction2 f = new Fraction2(3, 6);
+		f = f.reduce();
 		assertThat("1/2", is(f.toString()));
 	}
 
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testToString()
-	{
-		Fraction f = new Fraction(1, 2);
-		Fraction f1 = new Fraction(2, 4);
-		Fraction f2 = new Fraction(3);
-		
+	public void testToString() {
+		Fraction2 f = new Fraction2(1, 2);
+		Fraction2 f1 = new Fraction2(2, 4);
+		Fraction2 f2 = new Fraction2(3);
+
 		assertThat("1/2", is(f.toString()));
 		assertThat("2/4", is(f1.toString()));
 		assertThat("3", is(f2.toString()));
-		
+
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testConvertFloat()
-	{
-		Fraction f1 = new Fraction(1, 3);
+	public void testConvertFloat() {
+		Fraction2 f1 = new Fraction2(1, 3);
 		double floatPoint = f1.convertFloat();
 		assertThat(floatPoint, is(0.3333333333333333));
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test(expected = ArithmeticException.class)
 	public void testConvertInt() {
 
-		Fraction f1 = new Fraction(4, 2);
+		Fraction2 f1 = new Fraction2(4, 2);
 		int num = f1.convertInt();
 		assertThat(num, is(2));
 
-		Fraction f2 = new Fraction(1, 3);
-		int re = f2.convertInt();
+		Fraction2 f2 = new Fraction2(1, 3);
+		f2.convertInt();
 	}
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testEquals()
-	{
-		Fraction f1 = new Fraction(1, 2);
-		Fraction f2 = new Fraction(2, 4);
+	public void testEquals() {
+		Fraction2 f1 = new Fraction2(1, 2);
+		Fraction2 f2 = new Fraction2(2, 4);
 
 		assertThat(f1.equals(f1), is(true));
 		assertThat(f1.equals(f2), is(true));
 	}
-
 
 }
